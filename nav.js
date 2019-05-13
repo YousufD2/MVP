93673dea604068b835f432a0ca70b3155cb5d5a8
KahlZhang <473930123@qq.com>
2019-05-13 14:09:16 +0930

    Signed-off-by: KahlZhang <473930123@qq.com>

---
+++ nav.js
@@ -0,0 +1,108 @@
+nction fun1(){
+    var obj = document.getElementsByClassName("content-left-wrap1");
+
+    // var obj3 = document.getElementsByClassName("content-left-wrap3");
+    var s = document.getElementById("s1");
+
+    if(obj[0].style.display=="none"){
+        obj[0].style.display="block";
+        s.innerHTML = "-";
+    }
+    else{
+        obj[0].style.display="none";
+        s.innerHTML = "+";
+    }
+
+}
+
+function fun2(){
+    var obj = document.getElementsByClassName("content-left-wrap2");
+
+    // var obj3 = document.getElementsByClassName("content-left-wrap3");
+    var s = document.getElementById("s2");
+
+    if(obj[0].style.display=="none"){
+        obj[0].style.display="block";
+        s.innerHTML = "-";
+    }
+    else{
+        obj[0].style.display="none";
+        s.innerHTML = "+";
+    }
+
+}
+
+function fun3(){
+    var obj = document.getElementsByClassName("content-left-wrap3");
+    var s = document.getElementById("s3");
+
+    if(obj[0].style.display=="none"){
+        obj[0].style.display="block";
+        s.innerHTML = "-";
+
+    }
+    else{
+        obj[0].style.display="none";
+        s.innerHTML = "+";
+    }
+}
+
+function fun4(){
+    var obj = document.getElementsByClassName("content-left-wrap4");
+    var s = document.getElementById("s4");
+
+    if(obj[0].style.display=="none"){
+        obj[0].style.display="block";
+        s.innerHTML = "-";
+
+    }
+    else{
+        obj[0].style.display="none";
+        s.innerHTML = "+";
+    }
+}
+
+function fun5(){
+    var obj = document.getElementsByClassName("content-left-wrap5");
+    var s = document.getElementById("s5");
+
+    if(obj[0].style.display=="none"){
+        obj[0].style.display="block";
+        s.innerHTML = "-";
+
+    }
+    else{
+        obj[0].style.display="none";
+        s.innerHTML = "+";
+    }
+}
+
+function fun6(){
+    var obj = document.getElementsByClassName("content-left-wrap6");
+    var s = document.getElementById("s6");
+
+    if(obj[0].style.display=="none"){
+        obj[0].style.display="block";
+        s.innerHTML = "-";
+
+    }
+    else{
+        obj[0].style.display="none";
+        s.innerHTML = "+";
+    }
+}
+
+function fun7(){
+    var obj = document.getElementsByClassName("content-left-wrap7");
+    var s = document.getElementById("s7");
+
+    if(obj[0].style.display=="none"){
+        obj[0].style.display="block";
+        s.innerHTML = "-";
+
+    }
+    else{
+        obj[0].style.display="none";
+        s.innerHTML = "+";
+    }
+}
