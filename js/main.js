6735678cb3369457c0978e4b245236a1e6abf1c3
KahlZhang <473930123@qq.com>
2019-05-17 02:27:33 +0930

    Signed-off-by: KahlZhang <473930123@qq.com>

--- js/main.js
+++ js/main.js
@@ -12,13 +12,13 @@ $(document).ready(function () {
 });
 
 function openNav() {
-    document.getElementById("mySidebar").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
+  document.getElementById("mySidebar").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
 }
 
 function closeNav() {
-    document.getElementById("mySidebar").style.width = "0";
-    document.getElementById("main").style.marginLeft= "0";
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft= "0";
 }
 
 function find() {
